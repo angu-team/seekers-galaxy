a81af66e6e89317713bcfe5d4e85efdbac33eae8
moinho <leandro.salustriano@hotmail.com>
2024-12-30 21:43:07 -0300

    feat(redis): andromeda sera responsavel pelo redis

--- src/repositories/ConfigurationRepository2.ts
+++
@@ -1,7 +0,0 @@
-import {RedisRepository} from "./redis/RedisRepository";
-import {labelType} from "./redis/redis-repository";
-import {ElasticUtils} from "../utils/ElasticUtils";
-
-export class ConfigurationRepository2 {
-    private redis:RedisRepository = new RedisRepository();
-}
