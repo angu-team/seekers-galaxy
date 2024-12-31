09212d4279facfa88b17d226085c29b5e3d3791b
moinho <leandro.salustriano@hotmail.com>
2024-12-30 22:39:47 -0300

    redesign

--- src/Main.ts
+++ src/Main.ts
@@ -2,12 +2,17 @@ import {EthersRepository} from "./repositories/andromeda/EthersRepository";
 import dotenv from "dotenv"
 import {EthersController} from "./controllers/andromeda/EthersController";
 import {TelebotRouter3} from "./TelebotRouter3";
+import {Router} from "./Router";
+import {AxiosClient} from "./AxiosClient";
 dotenv.config()
 
 const ethersRepository = new EthersRepository(process.env.ANDROMEDA_URL!);
 
 (() => {
+    AxiosClient.initLogger();
+
     new EthersController(ethersRepository);
 
     TelebotRouter3.initialize(process.env.TELEBOT_TOKEN!)
+    Router.initialize(process.env.SERVER_PORT!)
 })()
