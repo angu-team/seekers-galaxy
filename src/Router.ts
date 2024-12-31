c61b8c8ba79f5d0e1883a2af559b790ed20dcf87
moinho <leandro.salustriano@hotmail.com>
2024-12-30 23:13:56 -0300

    chore(router): logging

--- src/Router.ts
+++ src/Router.ts
@@ -1,4 +1,4 @@
-import express, {Express,Request} from "express";
+import express, {Express, Request} from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 
@@ -6,39 +6,45 @@ type methods = "get" | "post" | "put" | "delete"
 
 export class Router {
 
-    private static express:Express = express()
+    private static express: Express = express()
         .use(morgan(this.logRequest))
-        .use(bodyParser.urlencoded({ extended: false }))
-        .use(bodyParser.json())
+        .use(bodyParser.urlencoded({extended: false}))
+        .use(bodyParser.json());
 
-    public static initialize(port:string){
-        morgan.token('body', (req:Request) => {
+    public static initialize() {
+        const PORT = 8081
+
+        morgan.token('body', (req: Request) => {
             return JSON.stringify(req.body)
         })
 
-        morgan.token('identifier', req => {
-            return JSON.stringify(req.headers.identifier)
+        Router.express.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
         })
+            .on("error", (e) => {
+                throw new Error(e.message)
+            })
+            .on("disconnect", (e) => {
+                throw new Error(e.message)
+            })
 
-        Router.express.listen(port).on("connect",() => {
-            console.info("server listening on port", port)
-        })
     }
 
-    public static RequestMapping(endpoint: string,method:methods) {
+    public static RequestMapping(endpoint: string, method: methods) {
 
         return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
             const originalMethod = descriptor.value;
 
-            descriptor.value = async function (req:Request) {
+            descriptor.value = async function (req: Request) {
                 return await originalMethod.call(this, req);
             };
             Router.express[method](`/${endpoint}`, async function (req, res) {
                 const serviceResponse = await descriptor.value(req)
 
-                if(serviceResponse instanceof  Error) {
+                if (serviceResponse instanceof Error) {
                     return res.status(400).send(String(serviceResponse))
-                };
+                }
+                ;
 
                 return res.status(200).json(serviceResponse)
             })
@@ -46,7 +52,7 @@ export class Router {
         };
     }
 
-    private static logRequest(tokens:any, req:any, res:any){
+    private static logRequest(tokens: any, req: any, res: any) {
         console.info(`[${new Date().toLocaleString("pt-BR")}] ${tokens.method(req, res)}:: ${tokens.url(req, res)} - ${tokens.status(req, res)} - ${tokens['response-time'](req, res)} 'ms'`);
         return ""
     }
