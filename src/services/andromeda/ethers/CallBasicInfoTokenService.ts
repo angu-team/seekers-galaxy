029319682b7d94abd0fe9f352d733eb12cb34423
moinho <leandro.salustriano@hotmail.com>
2024-12-31 23:11:50 -0300

    feat(ethers): service para obter as funções basicas de um token erc20

---
+++ src/services/andromeda/ethers/CallBasicInfoTokenService.ts
@@ -0,0 +1,73 @@
+import {EthersRepository} from "../../../repositories/andromeda/EthersRepository";
+
+export class CallBasicInfoTokenService {
+    private readonly functions = [
+        "name","symbol","decimals","totalSupply"
+    ]
+
+    private readonly abi =         [
+        {
+            "inputs": [],
+            "name": "name",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "symbol",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "decimals",
+            "outputs": [
+                {
+                    "internalType": "uint8",
+                    "name": "",
+                    "type": "uint8"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "totalSupply",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ]
+
+    constructor(private repository:EthersRepository) {
+    }
+
+    async exec(userId:number,address:string){
+        let abi = JSON.stringify(this.abi);
+        let call:any = await this.repository.callFunctions(userId,address,this.functions,abi);
+
+        return call;
+    }
+
+}
