24a868e48e0c56bb6fb7f18b5a6071f621fd867c
moinho <leandro.salustriano@hotmail.com>
2025-01-02 02:51:02 -0300

    chore(date-utils): manipulaçao do timestamp

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -11,4 +11,29 @@ export class DateUtils {
         return differenceInMilliseconds / 1000
     }
 
+    public static formatTimestamp(timestamp: number): string {
+        const date = new Date(timestamp * 1000);
+        const now = new Date();
+
+        const diffInMs = now.getTime() - date.getTime();
+        const diffInSeconds = Math.floor(diffInMs / 1000);
+
+        const days = Math.floor(diffInSeconds / (3600 * 24));
+        const hours = Math.floor((diffInSeconds % (3600 * 24)) / 3600);
+
+        // const minutes = Math.floor((diffInSeconds % 3600) / 60);
+        // const seconds = diffInSeconds % 60;
+
+        const formattedTime = date.toLocaleTimeString("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+        });
+
+
+        const relativeTime = days > 0 ? `${days} days ago` : `${hours} hours ago`;
+        return `${relativeTime}, ${formattedTime}`;
+    }
+
+
 }
