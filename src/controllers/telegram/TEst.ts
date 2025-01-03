5acdd9843f7250105018003bffa4cf867a84b49f
moinho <leandro.salustriano@hotmail.com>
2025-01-03 16:03:32 -0300

    "Adiciona serviço `MugetsoCommandSequenceFeedService` para processar mensagens recebidas e atualiza método `test` para utilizar ID e texto da mensagem."

--- src/controllers/telegram/TEst.ts
+++ src/controllers/telegram/TEst.ts
@@ -1,10 +1,16 @@
 import {TelegramServer} from "../TelegramServer";
+import {MugetsoCommandSequenceFeedService} from "../../services/bot/MugetsoCommandSequenceFeedService";
 
 export class TEst {
+    private static service:MugetsoCommandSequenceFeedService;
 
-    @TelegramServer.ReceiveMessage(93372553)
-    test(message:string) {
-        console.log(message);
+    constructor(service:MugetsoCommandSequenceFeedService) {
+        TEst.service = service;
+    }
+
+    @TelegramServer.ReceiveMessage(7069767637)
+    test(messageId:number,message:string) {
+        TEst.service.listenMessages(messageId.toString(),message)
     }
 
 }
