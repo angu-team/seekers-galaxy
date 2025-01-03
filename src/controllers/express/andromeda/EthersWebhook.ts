347e5a5b92a4076565fdff4bdfc8b15efd1cdd49
moinho <leandro.salustriano@hotmail.com>
2025-01-02 22:12:38 -0300

    Add handler for listening to contract events in EthersWebhook
    Introduced `ListenContractEventsHandler` in `EthersWebhook` to process contract event requests. Updated the constructor and request mapping to integrate this new handler. This enhances the webhook's functionality to support additional event types.

--- src/controllers/express/andromeda/EthersWebhook.ts
+++ src/controllers/express/andromeda/EthersWebhook.ts
@@ -1,12 +1,15 @@
 import {ExpressServer} from "../../ExpressServer";
 import {Request} from "express";
 import {ListenDeployErc20Handler} from "../../../services/bot/handlers/ListenDeployErc20Handler";
+import {ListenContractEventsHandler} from "../../../services/bot/handlers/ListenContractEventsHandler";
 
 export class EthersWebhook {
     private static listenDeployErc20Webhook:ListenDeployErc20Handler;
+    private static listenContractEventsWebhook:ListenContractEventsHandler;
 
-    constructor(listenDeployErc20Webhook:ListenDeployErc20Handler) {
+    constructor(listenDeployErc20Webhook:ListenDeployErc20Handler,listenContractEventsWebhook:ListenContractEventsHandler) {
         EthersWebhook.listenDeployErc20Webhook = listenDeployErc20Webhook;
+        EthersWebhook.listenContractEventsWebhook = listenContractEventsWebhook
     }
 
     @ExpressServer.RequestMapping("ethers/listen_deploy_erc20","post")
@@ -16,7 +19,7 @@ export class EthersWebhook {
 
     @ExpressServer.RequestMapping("ethers/listen_contract_events","post")
     listenContractEventsWebhook(request:Request) {
-        console.log(request.body);
+        EthersWebhook.listenContractEventsWebhook.handle(request.body)
     }
 
 }
